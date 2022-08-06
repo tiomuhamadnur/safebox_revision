36bfe09ed956063018d005476c293c853859d723
Dede Irfan <dedeirfan@Dedes-MacBook-Pro.local>
2022-08-06 15:06:46 +0700

    dashboard tambah wadah

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -10,7 +10,6 @@
             </div>
             <a class="navbar-brand" href="#safebox">
                 <img class="logo-image-small" style="max-height: 50px" src="{{ asset('paper') }}/img/safebox_logo_white.png">
-               
             </a>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
